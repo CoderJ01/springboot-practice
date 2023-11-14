654f8efcfd3c477b849fdc37426921d203644012
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-14 15:31:40 -0800

    display list with a single student on browser (8080)

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Month;
 import java.util.*;
+import java.time.LocalDate;
+
 
 @SpringBootApplication
 @RestController // allows class to server REST endpoints
@@ -16,7 +21,9 @@ public class DemoApplication {
 
 	// hello() is a REST endpoint
  	@GetMapping // allows method to be served
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L,"Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
+		);
 	}
 }
