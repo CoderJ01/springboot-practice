a572ece7bd331e6a57bf09de7860bad13a46cae9
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-14 14:37:55 -0800

    display 'Hello World' on browser via 8080 port

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // allows class to server REST endpoints
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	// hello() is a REST endpoint
+ 	@GetMapping // allows method to be served
+	public String hello() {
+		return "Hello World";
+	}
 }
