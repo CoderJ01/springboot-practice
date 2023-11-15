89ea518c8a4f5cc93c23c3574de529971c7bb1db
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-14 20:46:45 -0800

    remove application properties to for password protection

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,22 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity // for "hibernate" 41:05
+@Table // used for table in database
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
