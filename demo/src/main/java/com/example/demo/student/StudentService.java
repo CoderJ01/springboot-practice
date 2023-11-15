ee54a7e41140c2d81e8fdcf1c61f034017319712
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-15 12:53:44 -0800

    add JPA repository

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +11,16 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentReopistory;
+
+    @Autowired
+    public StudentService(StudentRepository studentReopistory) {
+        this.studentReopistory = studentReopistory;
+    }
+
     // REST endpoint
     @GetMapping // allows method to be served
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L,"Mariam", "mariam.jamal@gmail.com", LocalDate.of(2000, Month.JANUARY, 5), 21)
-        );
+        return studentReopistory.findAll();
     }
 }
